2264f366bdaf69ac70737f9324796e1b2cbf4206
= <6349874+senegrom@users.noreply.github.com>
2021-12-26 23:27:41 +0000

    update

--- run.py
+++ run.py
@@ -2,22 +2,20 @@ import argparse
 import importlib
 import time
 
-import examples
+import examples2
 from gridsolver.abstract_grids import solver
 from gridsolver.abstract_grids.grid_loading import create_from_str
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Solve grid puzzle")
-    group = parser.add_mutually_exclusive_group()
-    group_str = group.add_argument_group()
-    group.add_argument("-f", help="module file to load puzzle from", type=str, nargs="?")
-    group_str.add_argument("-s", "--str", help="string to load puzzle from", type=str)
-    group_str.add_argument("-c", "--class_", help="puzzle class", choices=("sudoku", "killersudoku", "futoshiki"),
-                           type=str)
-    group.add_argument("-e", "--example", choices=("a", "b", "c", "d", "f", "m", "s", "t"),
-                       help="Choose one of the default example puzzles and do not load from module file",
-                       type=str, required=False)
+    parser.add_argument("-f", "--file", help="module file to load puzzle from", type=str, nargs="?")
+    parser.add_argument("-s", "--str", help="string to load puzzle from", type=str)
+    parser.add_argument("-c", "--class_", help="puzzle class", choices=("sudoku", "killersudoku", "futoshiki"),
+                        type=str)
+    parser.add_argument("-e", "--example", choices=("a", "b", "c", "d", "f", "m", "s", "t"),
+                        help="Choose one of the default example puzzles and do not load from module file",
+                        type=str, required=False)
     parser.add_argument("-d", "--detail", type=int,
                         help="Detail of log output (higher means more intermediate steps are shown)", required=False)
     parser.add_argument("-v", "--verbose", action="store_true", help="Print very detailed log output (every step)")
@@ -31,12 +29,19 @@ if __name__ == "__main__":
 
     start_time = time.process_time()
 
-    if args.f:
-        print(f"Importing grid puzzle from {args.f}")
-        x = importlib.import_module(args.f)
+    if args.file and args.example:
+        raise ValueError("Cannot choose from file and example.")
+    if args.file and args.str:
+        raise ValueError("Cannot choose from file and string.")
+    if args.example and args.str:
+        raise ValueError("Cannot choose from string and example.")
+
+    if args.file:
+        print(f"Importing grid puzzle from {args.file}")
+        x = importlib.import_module(args.file)
         solver.solve(x.g, detail)
     elif args.example:
-        g = examples.get_example(args)
+        g = examples2.get_example(args)
         solver.solve(g, detail)
     elif args.str:
         g = create_from_str(args.str, args.class_)
