e1ce0b3c16dc19f1213cd399ae5d8c4826e030fb
senegrom <6349874+senegrom@users.noreply.github.com>
2022-01-19 00:52:53 +0000

    update

--- tests/helpers.py
+++ tests/helpers.py
@@ -19,7 +19,7 @@ def solve_all_in_path(path: Path, space_sep: bool, max_count=0):
         lg.logs(0, f"\nLoading {file}")
         g = create_from_file(file, space_sep=space_sep)
         lg.logs(0, f"\nSolving {file}")
-        sol = solver.solve(g, -1)
+        sol = solver.solve(g, 100)
         assert len(sol) == 1
         counter += 1
 
