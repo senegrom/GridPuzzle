592b11f29544417e4bd483a60437b7e632cb66fe
senegrom <6349874+senegrom@users.noreply.github.com>
2026-08-11 05:19:37 +0100

    Remove temporary export scaffolding

--- gridsolver/abstract_grids/rule_container.py
+++ gridsolver/abstract_grids/rule_container.py
@@ -1,20 +1,22 @@
-from typing import Set
-
-from gridsolver.rules.rules import Rule, Guarantee
+from gridsolver.rules.rules import Guarantee, Rule
 
 
 class RuleContainer:
-    def __init__(self):
-        self.rules: Set[Rule] = set()
-        self.rules_ia: Set[Rule] = set()
-        self.guarantees: Set[Guarantee] = set()
-        self.guarantees_ia: Set[Guarantee] = set()
+    def __init__(self) -> None:
+        self.rules: set[Rule] = set()
+        self.rules_ia: set[Rule] = set()
+        self.guarantees: set[Guarantee] = set()
+        self.guarantees_ia: set[Guarantee] = set()
 
-    def __eq__(self, other: 'RuleContainer') -> bool:
+    def __eq__(self, other: object) -> bool:
         if not isinstance(other, type(self)):
             return False
-        return self.rules == other.rules and self.rules_ia == other.rules_ia and \
-               self.guarantees == other.guarantees and self.guarantees_ia == other.guarantees_ia
+        return (
+            self.rules == other.rules
+            and self.rules_ia == other.rules_ia
+            and self.guarantees == other.guarantees
+            and self.guarantees_ia == other.guarantees_ia
+        )
 
-    def __ne__(self, other: 'RuleContainer') -> bool:
+    def __ne__(self, other: object) -> bool:
         return not self == other
